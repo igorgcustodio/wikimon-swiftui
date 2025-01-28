81c8fe4f361a90eef2175af5440c22d9d0482eb8
Igor Custodio <igor.custodio@alayacare.com>
2025-01-28 18:13:32 -0300

    Initial Commit

---
+++ Wikimon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wikimon
+//
+//  Created by Igor Custodio on 28/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wikimon/WikimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikimonApp.swift
+//  Wikimon
+//
+//  Created by Igor Custodio on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WikimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
