db98b10e36c820acdb82d06ccbc0fb39a9b6dede
Igor Custodio <igor.custodio@alayacare.com>
2025-01-29 16:14:20 -0300

    feat: update models

--- Wikimon/Models/Species.swift
+++ Wikimon/Models/Species.swift
@@ -1,13 +1,29 @@
 import Foundation
 
 /// Response from the `getSpeciesList` endpoint
-struct SpeciesResponse: Decodable {
+struct SpeciesResponse: Networking.Resource {
     let count: Int
     let results: [Species]
 }
 
 /// Species object returned as part of the `SpeciesResponse` object from the `getSpeciesList` endpoint
-struct Species: Decodable {
+struct Species: Decodable, Identifiable {
+    let id = UUID()
     let name: String
     let url: URL
+
+    enum CodingKeys: CodingKey {
+        case name
+        case url
+    }
+
+    var specieIdentifier: Int? {
+        Int(url.lastPathComponent)
+    }
 }
+
+#if DEBUG
+struct MockSpecies {
+    static var mock = Species(name: "bulbasaur", url: URL(string: "https://pokeapi.co/api/v2/pokemon-species/1/")!)
+}
+#endif
--- Wikimon/Models/SpeciesDetails.swift
+++ Wikimon/Models/SpeciesDetails.swift
@@ -1,12 +1,73 @@
 import Foundation
 
 /// Species object returned as part of the `getSpeciesDetails` endpoint
-struct SpeciesDetails: Decodable {
-    let name: String
-    let evolutionChain: EvolutionChain
+struct SpeciesDetails: Networking.Resource {
+    let baseHappiness: Int?
+    let captureRate: Int?
+    let color: Color?
+    let eggGroups: [Color]?
+    let evolutionChain: EvolutionChain?
+    let evolvesFromSpecies: Color?
+    let flavorTextEntries: [FlavorTextEntry]?
+    let formsSwitchable: Bool?
+    let genderRate: Int?
+    let genera: [Genus]?
+    let generation: Color?
+    let growthRate: Color?
+    let habitat: Color?
+    let hasGenderDifferences: Bool?
+    let hatchCounter: Int?
+    let id: Int?
+    let isBaby: Bool?
+    let isLegendary: Bool?
+    let isMythical: Bool?
+    let name: String?
+    let names: [Name]?
+    let order: Int?
+    let palParkEncounters: [PalParkEncounter]?
+    let pokedexNumbers: [PokedexNumber]?
+    let shape: Color?
+    let varieties: [Variety]?
 }
 
 /// EvolutionChain model returned as part of the SpeciesDetails from the `getSpecies` endpoint
 struct EvolutionChain: Decodable {
     let url: URL
+}
+
+struct Color: Decodable {
+    let name: String?
+    let url: String?
+}
+
+struct FlavorTextEntry: Decodable {
+    let flavorText: String?
+    let language: Color?
+    let version: Color?
+}
+
+struct Genus: Decodable {
+    let genus: String?
+    let language: Color?
+}
+
+struct Name: Decodable {
+    let language: Color?
+    let name: String?
+}
+
+struct PalParkEncounter: Decodable {
+    let area: Color?
+    let baseScore: Int?
+    let rate: Int?
+}
+
+struct PokedexNumber: Decodable {
+    let entryNumber: Int?
+    let pokedex: Color?
+}
+
+struct Variety: Decodable {
+    let isDefault: Bool?
+    let pokemon: Color?
 }
