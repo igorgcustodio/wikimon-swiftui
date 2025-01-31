286501a2dacda4b941cf11c6520115444c88aec9
Igor Custodio <igor.custodio@alayacare.com>
2025-01-30 21:06:13 -0300

    feat: UI fixes and overall improvements

--- Wikimon/Scenes/Details/SpeciesDetailsView.swift
+++ Wikimon/Scenes/Details/SpeciesDetailsView.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
 struct SpeciesDetailsView: View {
-    @ObservedObject var viewModel: SpeciesDetailsViewModel
-    
+    @StateObject var viewModel: SpeciesDetailsViewModel
+
     var body: some View {
         VStack {
             ZStack {
@@ -36,21 +36,23 @@ struct SpeciesDetailsView: View {
             ZStack {
                 Rectangle()
                     .fill(.shade1)
+                    .frame(height: 100)
 
-                if !viewModel.evolutionSpecies.isEmpty {
+                if !$viewModel.evolutionSpecies.isEmpty {
                     List {
-                        ForEach(viewModel.evolutionSpecies) { species in
+                        ForEach(viewModel.evolutionSpecies, id: \.id) { species in
                             PokemonCard(species: species)
                         }
+                        .frame(height: 40)
                     }
                 } else {
                     Text("No Evolution Chain available")
                         .font(.body)
                         .padding()
                         .foregroundStyle(.black)
+                        .frame(height: 100)
                 }
             }
-            .frame(height: 100)
 
             Spacer()
         }
